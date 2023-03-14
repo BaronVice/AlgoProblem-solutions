b1201b8888d7f349000f03ee1d2b38b74fdf03ce
BaronVice <toni.sas.228@mail.ru>
2023-03-14 16:32:58 +0800

    binsearch + 1613C.py

---
+++ python/1613C.py
@@ -0,0 +1,32 @@
+
+
+
+def damage(k, attacks, n):
+    total_damage = k
+    for i in range(1, n):
+        total_damage += min(attacks[i] - attacks[i-1], k)
+    return total_damage
+
+
+def get_result():
+    n, h = map(int, input().split())
+    attacks = list(map(int, input().split()))
+
+    left, right = 0, h
+    while right - left > 1:
+        k = (right + left) // 2
+        if damage(k, attacks, n) >= h:
+            right = k
+        else:
+            left = k
+
+    return right
+
+
+def main():
+    for _ in range(int(input())):
+        print(get_result())
+
+
+if __name__ == "__main__":
+    main()
