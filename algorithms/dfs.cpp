e2062f13f0744bee732d79b3f64d359f1aef1d34
Anton <96856582+BaronVice@users.noreply.github.com>
2024-03-25 14:01:03 +0800

    Baby, I'm just Ken (nobody else, nobody else)

---
+++ algorithms/dfs.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Списки смежности
+vector<vector<int>> g;
+// Посещенные
+vector<bool> visited;
+// Количество вершин и ребер
+int n, m;
+
+void dfs(int point){
+    // Заходим в вершину и отмечаем ее посещенной
+    visited[point] = true;
+    // Для каждой вершины, в которую мы можем прийти из текущей: заходим в нее, если она не посещена
+    for (auto el : g[point]) if (! visited[el]) {
+        dfs(el);
+    }
+}
+
+int main(){
+    cin >> n >> m;
+    // В задачах обычно счет вершин начинается с 1 (но в массивах счет с 0). Поэтому сделаем resize не на n, а на n+1
+    g.resize(n+1);
+    visited.resize(n+1);
+
+    while(m--){
+        // Между какими вершинами есть ребро
+        int from, to; cin >> from >> to;
+        g[from].push_back(to);
+        g[to].push_back(from);
+    } 
+
+    // Начнем обход с первой вершины
+    dfs(1);
+
+    return 0;
+}
