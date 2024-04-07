a85f9af7df86ae794d555e47a5aa54a96a905d42
Anton <96856582+BaronVice@users.noreply.github.com>
2024-04-07 16:54:30 +0800

    My name's Ken (and so am I)

--- algorithms/dijkstra.cpp
+++ algorithms/dijkstra.cpp
@@ -6,10 +6,15 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 
+// Количество вершин и стартовая
 int n, s;
+// Граф
 vector<vector<pii>> g;
+// Посещенные
 vector<bool> visited;
+// Кратчайшие расстояния от s до dist[i]
 vector<int> dist;
+// Как и в bfs, будем закидывать сюда вершины, но приоритет отдаем с меньшим расстоянием (first - вес, second - номер вершины)
 priority_queue<pii> q;
 
 void bfs(){
@@ -26,7 +31,9 @@ void bfs(){
             int to = p.second;
             int weight = p.first;
 
+            // Если от текущей могу перейти в соседнюю и улучшить ее расстояние
             if (dist[v] + weight < dist[to]){
+                // То улучшаю расстояние соседней вершины и закидываю ее на рассмотрение
                 dist[to] = dist[v] + weight;
                 q.push({dist[to], to});
             }
@@ -36,9 +43,9 @@ void bfs(){
 
 int main(){
     cin >> n;
-    g.resize(n);
-    visited.resize(n);
-    dist.resize(n, 1e9);
+    g.resize(n+1);
+    visited.resize(n+1);
+    dist.resize(n+1, 1e9 /* максимальное расстояние от s до любой другой (гарантируется в условии либо max(веса_ребер) * n) */);
 
     cin >> s;
     visited[s] = true;
