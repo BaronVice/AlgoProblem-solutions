ee6073140fb7201da895cd7334940614f791282e
Anton <96856582+BaronVice@users.noreply.github.com>
2024-04-16 15:15:10 +0800

    And I'm great at doing stuff

--- algorithms/dijkstra.cpp
+++ algorithms/dijkstra.cpp
@@ -1,10 +1,17 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
+
+// Функция сравнения (компаратор) для элементов priority_queue
+struct compare {
+   bool operator()(const pii& l, const pii& r){
+        // В очередь закидываем pii, pii.first - дистанция, pii.second - вершина, в которую идем
+        // Чем меньше дистанция, тем первее в очереди будет пара
+        return l.first > r.first;
+    }  
+};  
 
 // Количество вершин и стартовая
 int n, s;
@@ -15,12 +22,15 @@ vector<bool> visited;
 // Кратчайшие расстояния от s до dist[i]
 vector<int> dist;
 // Как и в bfs, будем закидывать сюда вершины, но приоритет отдаем с меньшим расстоянием (first - вес, second - номер вершины)
-priority_queue<pii> q;
+// compare - функция по которой определяется приоритет (здесь чем меньше значение - тем выше приоритет)
+priority_queue<pii, vector<pii>, compare> q;
 
 void bfs(){
     while (!q.empty()){
         int v = q.top().second;
         q.pop();
+
+        // cout << v << '\n';
 
         if (visited[v])
             continue;
@@ -41,14 +51,31 @@ void bfs(){
     }
 }
 
+void input(){
+    // Если граф задан матрицей смежности    
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            int d; cin >> d;
+            if (d == -1){
+                continue;
+            } if (i == j){
+                continue;
+            }
+
+            g[i].push_back({d, j});
+        }
+    }
+}
+
 int main(){
-    cin >> n;
+
+    cin >> n >> s;
     g.resize(n+1);
     visited.resize(n+1);
     dist.resize(n+1, 1e9 /* максимальное расстояние от s до любой другой (гарантируется в условии либо max(веса_ребер) * n) */);
 
-    cin >> s;
-    visited[s] = true;
+    input();
+
     dist[s] = 0;
     q.push({dist[s], s});
 
